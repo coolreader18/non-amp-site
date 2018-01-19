49d49c678852ac0bb0b5138ddf022ff469de37f8
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2018-01-19 10:26:02 -0600

    haha I'm silly

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const metascraper = require('metascraper');
 const got = require('got');
-module.exports = async function nonAmp(ampUrl) {
-  const {body: html, url} = await got(ampUrl);
-  return (await metascraper({html, url})).canonical;
+module.exports = function nonAmp(ampUrl) {
+  return new Promise(async res => {
+    const {body: html, url} = await got(ampUrl);
+    res((await metascraper({html, url})).canonical);
+  })
 };
