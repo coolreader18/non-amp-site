655bf10904b24ce144977ba25f13d024940ef94e
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2018-01-22 09:05:16 -0600

    works now, better

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-const metascraper = require('metascraper');
-const got = require('got');
-module.exports = function nonAmp(ampUrl) {
-  return new Promise(async res => {
-    const {body: html, url} = await got(ampUrl);
-    res((await metascraper({html, url})).canonical);
-  })
-};
+var got=require('got');module.exports=function nonAmp(ampUrl){return got(ampUrl).then(r=>new DOMParser().parseFromString(r.body,"text/html").querySelector("link[rel=canonical]").attributes.getNamedItem("href").value)}
